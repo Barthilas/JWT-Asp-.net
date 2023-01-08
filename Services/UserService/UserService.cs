f9b0966b49db9b3c068f242301402ac611472e3e
Barthilas-WORK <snetivy.martin@seznam.cz>
2023-01-08 19:45:06 +0100

    httpContext

---
+++ Services/UserService/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace JwtWebApi.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public UserService(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+        public string GetMyName()
+        {
+            var result = string.Empty;
+            if (_context.HttpContext != null)
+                result = _context.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+
+            return result;
+        }
+    }
+}
